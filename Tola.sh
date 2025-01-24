eff6f6a3ec5a954b63f174daca3c682a822d14e6
Octavio <octosion.malware12@gmail.com>
2025-01-24 11:23:21 -0400

    Tola

---
+++ Tola.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Activar el entorno virtual
+source ~/miniconda3/bin/activate
+
+# Ejecuta el script de Python
+python /home/siles/comparar/InicioV/ProyectoVosr.py
+
+# Permiso de ejecucion del archivo
+chmod +x Tola.sh
+
+# Ejecutar el archivo
+./Tola.sh
